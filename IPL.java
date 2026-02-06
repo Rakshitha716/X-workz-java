b7241e1ab09472c4d35f710f10de0fb7953d1a86
Rakshitha716 <rakshithadarshan62@gmail.com>
2026-02-06 12:35:08 +0530

    adding variables related assisgnment files

--- IPL.java
+++
@@ -1,8 +0,0 @@
-class Ipl{
-
-public static void main(String[] ipl){
-String iplTeams[]={"RCB","CSK", "SRH", "KKR" , "DC", "GT", "PBKS", "LSG", "MI", "RR"}
-String iplTeam1= "RCB";
-String iplTeam2= "CSK";
-
-
